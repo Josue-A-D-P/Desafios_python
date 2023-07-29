559d5be6da99a1460f17e81a9606d6039c2c99bd
Josue <josueporto59@gmail.com>
2023-07-29 20:34:47 -0300

    Aumento de salario

--- Desafio48(Aumento_de_salario).py
+++ Desafio48(Aumento_de_salario).py
@@ -1,24 +1,24 @@
 
 
-salario = float(2000.00)
+salario = float(input())
 
-if salario <= 400.00:
+if salario <= 400.0:
     percentual = 15
 
-elif salario <= 800.00:
+elif salario <= 800.0:
     percentual = 12
 
-elif salario <= 1200.00:
+elif salario <= 1200.0:
     percentual = 10
 
-elif salario <= 2000.00:
+elif salario <= 2000.0:
     percentual = 7
 else:
     percentual = 4
 
-reajuste = percentual * salario / 100
-novo_salario = reajuste + salario
+reajuste = salario * percentual / 100.0
+novo_salario = salario + reajuste
 
 print(f'Novo salario: {novo_salario:.2f}')
 print(f'Reajuste ganho: {reajuste:.2f}')
-print('Em percentual:', percentual, '%')
+print(f'Em percentual: {percentual} %')
